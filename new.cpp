4882f0801f2deaba15e99b56d05eba11ede5be78
Natalia <73670894+natfb@users.noreply.github.com>
2023-03-15 17:49:21 -0300

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string name("joe");
+    int find = name.find("o");
+
+    cout << "hello world" << endl;
+    cout << "hello " << name << find << endl;
+
+    return 0;
+}
+
