4fa87945f82e08a12a82c9dd868a3575715e1c48
Natalia <natifari4s@gmail.com>
2023-03-29 10:58:24 -0300

    count characters

--- aula27-03.cpp
+++ aula27-03.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+#include <utility>
 
 using namespace std;
 
 bool Readfile(string path, string &content);
+
+//count frequency
+bool countFreq(string content, vector<pair<int, char>> &ocurrances);
 
 int main(){
 
@@ -11,9 +17,15 @@ int main(){
 
     string fileContent;
 
+
+    vector< pair <int, char> > ocurrances;
+
     if (Readfile(filename, fileContent)){
         cout << "file was read." << endl;
         cout << fileContent;
+
+        countFreq(fileContent, ocurrances);
+
 
     } else {
          cout << "file was not read." << endl;
@@ -38,3 +50,27 @@ bool Readfile(string path, string &content){
 }
 
 // contar a frequencia de cada caracter
+bool countFreq(string content, vector<pair<int, char>> &ocurrances){
+
+    int c = 0;
+    vector <int> ascii;
+
+    for(int i = 0; i < 228; i++){
+        ascii.push_back(i);
+    }
+    ocurrances = {};
+    for (int i = 0; i < content.length(); i++){
+        for(int j = 0; j < 228; j++){
+            if(content[i] == ascii[j]){
+                cout << "aqui";
+                char ch = j;
+                cout << "aqui 2" << ch << "," << j;
+                ocurrances[c].first = j;
+                cout << "aqui 2.5" << ch << "," << j;
+                ocurrances[c].second = ch;
+                cout << "aqui 3";
+                c++;
+            }
+        }
+    }
+}
