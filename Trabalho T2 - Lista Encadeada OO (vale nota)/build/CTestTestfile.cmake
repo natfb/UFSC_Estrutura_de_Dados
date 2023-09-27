b81c7c465f073eecb3d5554fc4b62896c826cb97
Natalia <natifari4s@gmail.com>
2023-09-26 21:19:59 -0300

    .

--- Trabalho T2 - Lista Encadeada OO (vale nota)/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Documentos/GitHub/UFSC_Estrutura_de_Dados/Trabalho T2 - Lista Encadeada OO (vale nota)
-# Build directory: D:/Documentos/GitHub/UFSC_Estrutura_de_Dados/Trabalho T2 - Lista Encadeada OO (vale nota)/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
