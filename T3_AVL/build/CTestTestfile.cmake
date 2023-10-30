8c8c721d14d25ad179d7fc567768401305db12f3
Natalia <73670894+natfb@users.noreply.github.com>
2023-10-30 19:52:03 -0300

    Delete T3_AVL/build directory

--- T3_AVL/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/aluno/Downloads/UFSC_Estrutura_de_Dados-main (1)/UFSC_Estrutura_de_Dados-main/T3_AVL
-# Build directory: D:/aluno/Downloads/UFSC_Estrutura_de_Dados-main (1)/UFSC_Estrutura_de_Dados-main/T3_AVL/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
